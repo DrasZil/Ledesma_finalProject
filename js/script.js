299a6abbf3bf4fc4c69c200d5170f4e66e651823
OliverLedesma <114034970+OliverLedesma@users.noreply.github.com>
2025-11-10 22:26:10 +0800

    -Added lightbox onclick functionality

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*Adding click events to make the 
+lightbox feature work in the projects page */
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.getElementById('close');
+
+//Selecting all project images
+document.querySelectorAll('.projects .slider .items img').forEach(img => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src; //set clicked image
+        lightbox.classList.add('active'); //show lightbox with fade style
+    });
+});
+
+//close lightbox on click
+closeBtn.addEventListener('click', () => {
+    lightbox.classList.remove('active');
+});
+
+//Can close by clicking outside the image
+lightbox.addEventListener('click', e =>{
+    if(e.target === lightbox) lightbox.classList.remove('active');
+});
