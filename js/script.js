c5998fa89ee4c789a646d302756f41fbdf867948
OliverLedesma <draszil.1720@gmail.com>
2025-11-23 23:35:38 +0800

    better integration for diff html files

--- js/script.js
+++ js/script.js
@@ -1,34 +1,44 @@
-/*Adding click events to make the 
-lightbox feature work in the projects page */
-const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-img');
-const closeBtn = document.getElementById('close');
+document.addEventListener('DOMContentLoaded', () => { 
+ //projects.css
+        /*Adding click events to make the 
+    lightbox feature work in the projects page */
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const closeBtn = document.getElementById('close');
 
-//Selecting all project images
-document.querySelectorAll('.projects .slider .items img').forEach(img => {
-    img.addEventListener('click', () => {
-        lightboxImg.src = img.src; //set clicked image
-        lightbox.classList.add('active'); //show lightbox with fade style
+    if(lightbox && lightboxImg && closeBtn){
+
+    //Selecting all project images
+    document.querySelectorAll('.projects .slider .items img').forEach(img => {
+        img.addEventListener('click', () => {
+            lightboxImg.src = img.src; //set clicked image
+            lightbox.classList.add('active'); //show lightbox with fade style
+        });
     });
+
+    //close lightbox on click
+    closeBtn.addEventListener('click', () => {
+        lightbox.classList.remove('active');
+    });
+
+    //Can close by clicking outside the image
+    lightbox.addEventListener('click', e =>{
+        if(e.target === lightbox) lightbox.classList.remove('active');
+    });
+
+    }
+
+    //for all html files
+    /*Dropdown menu for the website, this will be appear and 
+    be usable when the screen size is smaller */
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('.nav-bar');
+
+    if(menuIcon && navbar){
+        menuIcon.onclick = () => {
+            menuIcon.classList.toggle('bi-x-lg');
+            menuIcon.classList.toggle('bi-list');
+            navbar.classList.toggle('active');
+        }
+    }
 });
-
-//close lightbox on click
-closeBtn.addEventListener('click', () => {
-    lightbox.classList.remove('active');
-});
-
-//Can close by clicking outside the image
-lightbox.addEventListener('click', e =>{
-    if(e.target === lightbox) lightbox.classList.remove('active');
-});
-
-/*Dropdown menu for the website, this will be appear and 
-be usable when the screen size is smaller */
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.nav-bar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bi-x-lg');
-    menuIcon.classList.toggle('bi-list');
-    navbar.classList.toggle('active');
-}
